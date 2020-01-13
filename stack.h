4f920ab5505617094a45c1203d58af8b112059fd
amonbansod <54807815+amonbansod@users.noreply.github.com>
2020-01-13 20:53:57 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,90 +1,77 @@
 #include<iostream>
 using namespace std;
 
-template<class T>
+template<class T>			//template (data type may differ)
 class stack
 {
 	int i,top;
 	int max_size;
-	T s[20];
+	T s[20];			//declaing array 's'
 	
 public:
 
 	stack()
 	{
-		top=-1;
+		top=-1;				//initialising top and max_size
 		max_size=20;
 	}
 	
-	int is_empty()
+	int is_empty()				//checks whether the stack is empty or not
 	{
 		if(top==-1)
 		{	
-			return 1;
+			return 1;		//if empty it will return 1
 		}
 	
 		else
 		{
-			return 0;
+			return 0;		//if not empty it will return 0
 		}
 	}
 	
-	int is_full()
+	int is_full()				//checks whether the stack is fullor not
 	{
 		if(top=max_size-1)
 		{
-			return 1;
+			return 1;		//if stack is full it returns 1
 		}
 	
 		else
 		{
-			return 0;
+			return 0;		//if stack is not full it returns 0
 		}
 	}
 	
-	void push(T item )
+	void push(T item )			//function definition to push an element onto the stack
 	{
-		if(!is_full())
+		if(!is_full())			//checking whether the stack is full.(if not full add elements)
 		{
 			top++;
-			s[top]=item;
+			s[top]=item;		//element added to the location pointed by top
 		
 		}
 		else
 		{
-			cout<<"STACK OVERFLOW."<<endl;
+			cout<<"STACK OVERFLOW."<<endl;		//if stack is full, it will display the given message
 		}
 	}
 	
-	T pop()
+	T pop()							//function definition to pop an element from the stack
 	{
-		if(!is_empty())
+		if(!is_empty())					//checks whether the stack is empty or not.(if not empty then pop topmost element)
 		{
 			T item;
 			item=s[top];
-			top--;
+			top--;					//element poped
 			return item;
 		}
 	
 		else
 		{
-			cout<<"stack UNDERFLOW."<<endl;
+			cout<<"stack UNDERFLOW."<<endl;		//if stack is empty it will display the given message
 		}
 	}
 	
 };
-/*
-int stack::is_empty()
-
-
-int stack::is_full()
-
-
-int stack::push(int item)
-
-
-int stack::pop()
-
-*/
 
 	
